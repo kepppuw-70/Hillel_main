e6419d03aa739ba3c7ad5ce9c2bc19ce246189c1
kepppuw_70 <tsgdnepro@3g.ua>
2020-09-10 10:08:04 +0300

    Lesson 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Привет мир!</title>
+   <link rel="stylesheet" type="text/css" href="css/style1.css">
+  </head>
+  <body>
+ 
+<br>
+<h1>Привет мир!</h1>
+<?php
+    echo 'Hello World!';      
+?>
+<br>
+  </body>
+</html>
