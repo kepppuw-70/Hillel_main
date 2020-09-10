2889eed19554801b79d311e240e728e816723316
kepppuw_70 <tsgdnepro@3g.ua>
2020-09-10 10:17:43 +0300

    HomeWork 2

---
+++ table_div.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Домашнее задание №2 - div</title>
+    <link rel="stylesheet" type="text/css" href="css/style1.css">
+  </head>
+  <body>
+    
+  <nav class="menu">
+  <ul>
+    <li><a href="index.php">Главная</a></li>
+    <li><a href="table_table.php">Таблица-table</a></li>
+    <li><a href="#">Таблица-div</a></li>
+  </ul>
+</nav>
+<br><br><br>
+
+<h1>Таблица - div</h1>
+
+<div class="table">
+	<div class="row">
+		<div class="cell1">Заголовок 1</div>
+		<div class="cell1">Заголовок 2</div>
+		<div class="cell1">Заголовок 3</div>
+	</div>
+   <div class="row">
+		<div class="cell1">11</div>
+		<div class="cell1">12</div>
+		<div class="cell1">13</div>
+	</div>
+   <div class="row">
+		<div class="cell1">21</div>
+		<div class="cell1">22</div>
+		<div class="cell1">23</div>
+	</div>
+   <div class="row">
+		<div class="cell1">31</div>
+		<div class="cell1">32</div>
+		<div class="cell1">33</div>
+	</div>
+</div>     
+    
+  </body>
+</html>
