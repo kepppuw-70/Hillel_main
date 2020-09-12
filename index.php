e93c11ed51a728f2f9da65501c64c99952fbabbe
kepppuw_70 <tsgdnepro@3g.ua>
2020-09-12 22:27:12 +0300

    Add all task HW5

--- index.php
+++ index.php
@@ -65,11 +65,12 @@ $var = fopen("index.php", 'r');
 data_type_definition($var);
 echo '<br>';
 
-echo 'Не передаём переменнкю';
+echo 'Не передаём переменнкю'.'<br>';
 unset ($var);
 data_type_definition($var);
 echo '<br>';
-
+echo '<br>';
+/**/
 
 
 ?>
@@ -98,24 +99,65 @@ if (reed_b($string) != false) {
   } else {
     echo 'ошибка, передана не строка';
   }
-
   
-    
 ?>
 
 
 
 <h3>Создать функцию которая считает сумму значений всех элементов массива произвольной глубины</h3>
 
-
-
-
-1. чкркз рекурсию, вызывать саму себя если переменнаямассив. 
-
-2. через array_map и array_walk
-
-
 <?php
+
+echo 'Создаём массив:<br>';
+echo '<h3>$firstArr = [
+          <p>'."'".'one'."'".' => 1,'.'</p>
+          <p>'."'".'two'."'".' => ['.'</p>
+             <p class="mar">'."'".'one'."'".' => 1,'.'</p>
+             <p class="mar">'."'".'seven'."'".' => 22,'.'</p>
+             <p class="mar">'."'".'three'."'".' => 32,'.'</p>
+          <p>],</p>
+          <p>'."'".'three'."'".' => ['.'</p>
+             <p class="mar">'."'".'one'."'".' => 1,'.'</p>
+             <p class="mar">'."'".'two'."'".' => 2,'.'</p>
+          <p>],</p>
+          <p>'."'".'foure'."'".' => 5,'.'</p>
+          <p>'."'".'five'."'".' => ['.'</p>
+             <p class="mar">'."'".'three'."'".' => 32,'.'</p>
+             <p class="mar">'."'".'foure'."'".' => 5,'.'</p>
+             <p class="mar">'."'".'five'."'".' => 12,'.'</p>
+          <p>],</p>
+      ];
+      </h3>';
+
+$arr = [
+  'one' => 1,
+  'two' => [
+    'one' => 1,
+    'seven' => 22,
+    'three' => 32,
+  ],
+  'three' => [
+    'one' => 1,
+    'two' => 2,
+  ],
+  'foure' => 5,
+  'five' => [
+    'three' => 32,
+    'foure' => 5,
+    'five' => 12,
+  ],  
+];
+
+
+echo 'Ответ: ';
+
+echo 'сумма всех значений массива - '.count_sum_var_in_array($arr);
+
+echo '<br><br>';
+
+
+
+
 
 /*
 $firstArr = [
--- php_function/functionPHP.php
+++ php_function/functionPHP.php
@@ -2,40 +2,38 @@
 
 function data_type_definition($value)
 {    
-            $type = gettype($value);
-            if ($type == 'boolean') {
-                echo 'Тип введённой переменой - Булев.';
-            }
-            if ($type == 'integer') {
-                echo 'Тип введённой переменой - Целое.';
-            }
-            if ($type == 'double') {
-                echo 'Тип введённой переменой - Числа с плавающей точкой.';
-            }
-            if ($type == 'string') {
-                echo 'Тип введённой переменой - Строка.';
-            }
-            if ($type == 'array') {
-                echo 'Тип введённой переменой - Массив.';
-            }
-            if ($type == 'object') {
-                echo 'Тип введённой переменой - Обьект.';
-            }
-            if ($type == 'resource') {
-                echo 'Тип введённой переменой - Ресурс.';
-            }
-            if ($type == 'NULL') {
-                echo 'Состояние переменой - NULL.';
-            }
-            if ($type == 'unknown type') {
-                echo 'Тип введённой переменой - Неизвестен.';
-            }
-            
-            if (isset($value) != 1 ) {
-              echo 'Ошибка, переменная не переданна!.';
-            }  
- 
-            return ;
+    $type = 'NULL';
+    $type = gettype($value);
+    
+    switch ($type) {
+    case 'boolean':
+          echo 'Тип введённой переменой - Булев.';
+          break;
+    case 'integer':
+         echo 'Тип введённой переменой - Целое.';
+         break;
+    case 'double':
+         echo 'Тип введённой переменой - Числа с плавающей точкой.';
+         break;
+    case 'string':
+         echo 'Тип введённой переменой - Строка.';
+         break;
+    case 'array':
+         echo 'Тип введённой переменой - Массив.';
+         break;
+    case 'object':
+         echo 'Тип введённой переменой - Обьект.';
+         break;
+    case 'resource':
+         echo 'Тип введённой переменой - Ресурс.';
+         break;
+    case 'NULL':
+         echo 'Состояние переменой - NULL.';
+         break;
+    case 'unknown type':
+         echo 'Тип введённой переменой - Неизвестен.';
+         break;
+    }
 }
 
 
@@ -60,12 +58,20 @@ function reed_b($string)
 }
 
 
-function count_sum_var_in_array($arr)
+function count_sum_var_in_array($firstArr)
 {
-
-
-
+  foreach ($firstArr as $key => $value) {
+        if (is_array($firstArr[$key])) {
+            count_sum_var_in_array($firstArr[$key]);
+        } else {
+            static $sum;
+            $sum = $sum + $value;
+        }
+  }
+ return $sum;
 }
+
+
 
 
 function inscribing_squares($a, $b)
@@ -74,7 +80,7 @@ function inscribing_squares($a, $b)
   $x = (intval($a/$b))**2;
   echo 'Сторона первого квадрата - '.$a.'<br>';
   echo 'Сторона второго квадрата - '.$b.'<br>';
-   if ($x == 1) {
+  if ($x == 1) {
      echo 'В первый квадрат можно вписать - '.'1'. 'второй квадрат.';
   } else {
          if($x == 0){
@@ -84,13 +90,11 @@ function inscribing_squares($a, $b)
          }
       
   }
-
-  
-}
-if ($a < $b) {
-  $x = (intval($b/$a))**2;
-  echo 'Сторона первого квадрата - '.$a.'<br>';
-  echo 'Сторона второго квадрата - '.$b.'<br>';
+  }
+  if ($a < $b) {
+     $x = (intval($b/$a))**2;
+     echo 'Сторона первого квадрата - '.$a.'<br>';
+     echo 'Сторона второго квадрата - '.$b.'<br>';
   if ($x == 1) {
      echo 'Во второй квадрат можно вписать - '.'1'.' первыq квадрат';
   } else {
@@ -101,16 +105,12 @@ if ($a < $b) {
          }
       
   }
-  
-}
-/**/
-if ($a == $b) {
-  echo 'Сторона первого квадрата - '.$a.'<br>';
-  echo 'Сторона второго квадрата - '.$b.'<br>';
-  echo 'Квадраты равны, вписать нет возможности.';
-}
-   
-     return ;
+  }
+  if ($a == $b) {
+     echo 'Сторона первого квадрата - '.$a.'<br>';
+     echo 'Сторона второго квадрата - '.$b.'<br>';
+     echo 'Квадраты равны, вписать нет возможности.';
+  }
 }
 
 ?>
